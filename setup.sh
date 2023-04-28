8889dd48916207f5423e2c65beac642e0ffb575f
yyuujii <132074542+yyuujii@users.noreply.github.com>
2023-04-28 11:02:06 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"gabrielytr2007@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
